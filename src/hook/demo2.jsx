6a4f5fbc38a339810f44188ad1ade02dcdbeda95
R-Von <778615648@qq.com>
2020-01-16 16:19:01 +0800

    feat(src/hook/demo2.jsx): add new hooks demo2

--- src/hook/demo2.jsx
+++ src/hook/demo2.jsx
@@ -3,6 +3,7 @@ function Example(){
   const [ count , setCount ] = useState(0)
   return (
     <div>
+      <p>This is hook</p>
       <p>U clicked {count} times</p>
       <button onClick={()=>{setCount(count+1)}}>click me</button>
     </div>
