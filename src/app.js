f1c72411e5012d3529897f29f34867e2c0c1cbfe
R-Von <778615648@qq.com>
2020-01-17 16:51:39 +0800

    feat(src/app.js):add the react-router

--- src/app.js
+++ src/app.js
@@ -3,16 +3,31 @@ import React, { Component } from 'react'
 // 这里用的是ES6的语法——解构赋值  相当于
 // import React from 'react'
 // const Component = React.Component
-import Examples from './hook/demo4.jsx'
+// import Examples from './hook/demo4.jsx'
 import HookExample from './hook/demo5.jsx'
+import { BrowserRouter as Router , Route , Link } from 'react-router-dom'
+import Index from './hook/Index'
+import List from './hook/List'
+import Example4 from './hook/demo6'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Examples />
-        ------------------------------
-        <HookExample />
+        <Example4 />
+        {/* <HookExample /> */}
+        {/* <Router>
+          <ul>
+            <li>
+              <Link to="/">Index</Link>
+            </li>
+            <li>
+              <Link to="/list">List</Link>
+            </li>
+          </ul>
+          <Route path="/" exact component={Index} />
+          <Route path="/list" component={List} />
+        </Router> */}
       </div>
     )
   }
